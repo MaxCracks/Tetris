fdb405bb96fd2174530fd367328041e399fe7260
Chukwurado Umeaka <50035150+Chukwurado@users.noreply.github.com>
2019-12-12 18:22:48 -0500

    Add files via upload

--- src/sample/Tetris.java
+++ src/sample/Tetris.java
@@ -1,4 +1,3 @@
-
 package sample;
 
 import javafx.animation.Animation;
@@ -10,40 +9,69 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import sample.Pieces.Piece;
 
 import java.util.ArrayList;
-import java.util.List;
 
-
+/**
+ * The Tetris Class
+ */
 public class Tetris extends Application {
     private GameBoard gameBoard = new GameBoard();
     private Piece piece;
     private Pane gridPane = new Pane();
-    private Scene scene = new Scene(gridPane, gameBoard.WIDTH * gameBoard.GRIDSPACE, gameBoard.HEIGHT * gameBoard.GRIDSPACE);
-    private int score=0;
+    private Scene scene = new Scene(gridPane, gameBoard.WIDTH * gameBoard.GRIDSPACE + 150, gameBoard.HEIGHT * gameBoard.GRIDSPACE);
+    private int score = 0;
+    private Text scoreText = new Text("Score: ");
+    private int count;
+    private Timeline timeline;
+
 
     @Override
     public void start(Stage stage) throws Exception {
+        scoreText.setX(gameBoard.WIDTH * gameBoard.GRIDSPACE + gameBoard.GRIDSPACE);
+        scoreText.setY(gameBoard.GRIDSPACE);
+        gridPane.getChildren().add(scoreText);
         addToUI();
         stage.setScene(scene);
         stage.show();
-        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), ev -> {
+        timeline = new Timeline(new KeyFrame(Duration.seconds(1), ev -> {
             moveDown(piece);
         }));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
     }
 
+    /**
+     * This method adds the a Piece to the gridPane
+     */
     private void addToUI() {
+        count++;
         piece = gameBoard.makePiece();
-        gridPane.getChildren().addAll(piece.a, piece.b, piece.c, piece.d);
+        gridPane.getChildren().addAll(piece.getA(), piece.getB(), piece.getC(), piece.getD());
         moveOnKeyPress(piece);
+        scoreText.setText("Score: " + score);
+        score += 10;
+        //Increase the rate at which pieces move down as the game progresses
+        if(count > 50){
+            timeline.setRate(5);
+        }
+        else if (count > 35){
+            timeline.setRate(3);
+        }
+        else if(count > 10) {
+            timeline.setRate(2);
+        }
     }
 
+    /**
+     * This method adds an EventListener to a piece
+     * @param p - The newly generated Piece that is currently in motion
+     */
     private void moveOnKeyPress(Piece p){
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
@@ -66,6 +94,11 @@ public class Tetris extends Application {
         });
     }
 
+    /**
+     *This method moves a piece down using the moveDown from Gameboard.
+     * If a piece can not be moved, a new piece is generated and added to the board
+     * @param p - The piece being moved
+     */
     private void moveDown(Piece p){
         if(!gameBoard.moveDown(p)) {
             if(!gameBoard.updateBoard(p)){
@@ -77,6 +110,10 @@ public class Tetris extends Application {
         }
     }
 
+    /**
+     * This piece checks all the rows of the of the 2D grid from Gameboard
+     * If a row is completely true,
+     */
     private void checkRows() {
         int clearedRows = 0;
         for (int j=0;j<gameBoard.HEIGHT;j++) {
@@ -86,7 +123,7 @@ public class Tetris extends Application {
                     colFull++;
             }
             if (colFull==gameBoard.WIDTH) {
-            	clearedRows++;
+                clearedRows++;
                 gameBoard.clearRow(j);	//clear the row and drop above
                 clearRowsVisually(j);
             }
@@ -94,13 +131,19 @@ public class Tetris extends Application {
         addScore(clearedRows);
     }
 
+    /**
+     * This function removes all the rectangle from the row that is full and moves all rectangles above the rows down
+     * @param row - The row to be cleared
+     */
     private void clearRowsVisually(int row){
         ArrayList<Node> nodes = new ArrayList<>();
         //Get the nodes on the row that is full
         for(Node child : gridPane.getChildren()) {
-            Rectangle rec = (Rectangle) child;
-            if(rec.getY() / 25 == row) {
-                nodes.add(child);
+            if(child instanceof Rectangle){
+                Rectangle rec = (Rectangle) child;
+                if(rec.getY() / 25 == row) {
+                    nodes.add(child);
+                }
             }
         }
         //Remove the nodes from the pane
@@ -109,26 +152,28 @@ public class Tetris extends Application {
         }
         //Move the nodes above the row that is full 1 step down
         for(Node child : gridPane.getChildren()) {
-            Rectangle rec = (Rectangle) child;
-            if(rec.getY() / 25 < row) {
-                rec.setY(rec.getY() + gameBoard.GRIDSPACE);
+            if(child instanceof Rectangle){
+                Rectangle rec = (Rectangle) child;
+                if(rec.getY() / 25 < row) {
+                    rec.setY(rec.getY() + gameBoard.GRIDSPACE);
+                }
             }
+
         }
     }
-    
-    public void addScore(int clearedRows) {
-		if (clearedRows==1)
-			score+=40;
-		if (clearedRows==2)
-			score+=100;
-		if (clearedRows==3)
-			score+=300;
-		if (clearedRows==4)
-			score+=1200;
-	/*	else if (clearedRows!=0)
-		{
-			//throw exception here should not be possible
-		}
-		*/
-	}
+
+    /**
+     * This method adds a number to the players score based on the number of rows cleared
+     * @param clearedRows - the number of rows cleared
+     */
+    private void addScore(int clearedRows) {
+        if (clearedRows==1)
+            score+=50;
+        if (clearedRows==2)
+            score+=100;
+        if (clearedRows==3)
+            score+=300;
+        if (clearedRows==4)
+            score+=1200;
+    }
 }
